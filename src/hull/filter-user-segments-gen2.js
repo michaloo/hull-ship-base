c2a1c73e30b056c1742c3f3d5d9f8547dbbd1698
Michal Raczka <michal@raczka.me>
2017-02-14 12:57:01 +0100

    Adds new filtering

--- src/hull/filter-user-segments-gen2.js
+++ src/hull/filter-user-segments-gen2.js
@@ -2,15 +2,13 @@ import _ from "lodash";
 
 /**
  * Returns information if the users should be sent in outgoing sync.
- * This version - gen1 - includes all users if the setting is empty
+ * This version should filter out all users if the `synchronized_segments`
+ * setting is empty
  * @param  {Object} req Request Object
  * @param  {Object} user Hull user object
  * @return {Boolean}
  */
-export default function filterUserSegments(req, user) {
-  const filterSegmentIds = req.hull.ship.private_settings.synchronized_segments || [];
-  if (filterSegmentIds.length === 0) {
-    return true;
-  }
+export default function filterUserSegmentsGen2(req, user) {
+  const filterSegmentIds = _.get(req, "hull.ship.private_settings.synchronized_segments", []);
   return _.intersection(filterSegmentIds, user.segment_ids).length > 0;
 }
--- src/hull/index.js
+++ src/hull/index.js
@@ -8,6 +8,7 @@ export getAvailableProperties from "./get-available-properties";
 
 export setUserSegments from "./set-user-segments";
 export filterUserSegments from "./filter-user-segments";
+export filterUserSegmentsGen2 from "./filter-user-segments-gen2";
 
 export segmentsMiddleware from "./segments-middleware";
 export agentMiddleware from "./agent-middleware";
